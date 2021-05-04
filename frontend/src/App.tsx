382af84c13b6dbeec37337cb67abed9f06c0f1bf
aaanm13 <aaanm1309.1@gmail.com>
2021-05-04 12:20:26 -0300

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+        <p>
+          Ola Adriano.
+        </p>
+
+    </div>
+  );
+}
+
+export default App;
