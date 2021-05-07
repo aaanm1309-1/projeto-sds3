8721d69b7fac562ed3205adf8dcf89c4028aa130
aaanm13 <aaanm1309.1@gmail.com>
2021-05-07 16:37:09 -0300

    Group by search

--- backend/src/main/java/com/adrianomenezes/vendas/repositories/SaleRepository.java
+++ backend/src/main/java/com/adrianomenezes/vendas/repositories/SaleRepository.java
@@ -1,9 +1,25 @@
 package com.adrianomenezes.vendas.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.adrianomenezes.vendas.dto.SaleSuccessDTO;
+import com.adrianomenezes.vendas.dto.SaleSumDTO;
 import com.adrianomenezes.vendas.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long>{
+	
+	@Query("SELECT new com.adrianomenezes.vendas.dto.SaleSumDTO(obj.seller, SUM(obj.amount))"
+			+ " FROM Sale AS obj "
+			+ " GROUP BY obj.seller")
+	List<SaleSumDTO> amountGroupedBySeller();
+
+	@Query("SELECT new " 
+			+ " com.adrianomenezes.vendas.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited) , SUM(obj.deals))"
+			+ " FROM Sale AS obj "
+			+ " GROUP BY obj.seller")
+	List<SaleSuccessDTO> successGroupedBySeller();
 
 }
--- backend/src/main/java/com/adrianomenezes/vendas/services/SaleService.java
+++ backend/src/main/java/com/adrianomenezes/vendas/services/SaleService.java
@@ -1,6 +1,8 @@
 package com.adrianomenezes.vendas.services;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,6 +10,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.adrianomenezes.vendas.dto.SaleDTO;
+import com.adrianomenezes.vendas.dto.SaleSuccessDTO;
+import com.adrianomenezes.vendas.dto.SaleSumDTO;
 import com.adrianomenezes.vendas.entities.Sale;
 import com.adrianomenezes.vendas.repositories.SaleRepository;
 import com.adrianomenezes.vendas.repositories.SellerRepository;
@@ -26,4 +30,21 @@ public class SaleService {
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
 	}
+	
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller(){
+//		sellerRepository.findAll();
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller(){
+//		sellerRepository.findAll();
+		return repository.successGroupedBySeller();
+	}
+	
+	
+	
+	
 }
